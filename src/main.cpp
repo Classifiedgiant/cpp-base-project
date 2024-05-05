7f31a6f760764e5994ac974cb52987d5f6e27d94
Tom Lockie <lockie@192.168.1.116>
2024-05-05 18:23:38 +0100

    Use More appealing .clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <print>
 
-auto main(int argc, const char *argv[]) -> int {
-  std::cout << "Hello World\n";
+auto main(int argc, const char* argv[]) -> int {
+  std::print("Hello World\n");
+
   return 0;
 }
